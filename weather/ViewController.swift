1f7b53b2ae5eb007968aa8a4e4b086241b8e21fb
Eden <you8inpark@gmail.com>
2024-12-05 12:03:59 +0900

    feat: 최소, 최고 온도 레이블 생성

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -13,14 +13,29 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "서울특별시"
         label.textColor = .white
-        label.font = .systemFont(ofSize: 30)
+        label.font = .boldSystemFont(ofSize: 30)
         return label
     }()
     
     private let tempLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
-        label.font = .systemFont(ofSize: 50)
+        label.font = .boldSystemFont(ofSize: 50)
+        return label
+    }()
+    
+    private let tempMinLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    private let tempMaxLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
     }()
     
     override func viewDidLoad() {
