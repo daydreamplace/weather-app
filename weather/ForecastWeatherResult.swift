546f91ebd4a8df1c70f6e71ac2ea963909c5ed0e
Eden <you8inpark@gmail.com>
2024-12-05 11:56:17 +0900

    feat: 메인 스토리보드 삭제 후 프로젝트 시작점 재설정

---
+++ weather/ForecastWeatherResult.swift
@@ -0,0 +1,8 @@
+//
+//  ForecastWeatherResult.swift
+//  weather
+//
+//  Created by eden on 12/5/24.
+//
+
+import Foundation
---
+++ weather/TableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  TableViewCell.swift
+//  weather
+//
+//  Created by eden on 12/5/24.
+//
+
+import Foundation
