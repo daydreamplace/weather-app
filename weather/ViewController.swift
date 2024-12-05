6fd73b88e35afe8e2837633fbb292e15c28b9da5
Eden <you8inpark@gmail.com>
2024-12-05 11:59:37 +0900

    feat: 제목 레이블 생성

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "서울특별시"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 30)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
